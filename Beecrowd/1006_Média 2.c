08dbaa85fd9e38578e46f385cd717032d5a9757c
TinkyWinx <reservawinne@gmail.com>
2026-05-17 20:01:02 -0300

    Create 1006_Média 2.c

---
+++ Beecrowd/1006_Média 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    double a,b,c,media;
+    scanf("%lf%lf%lf",&a,&b,&c);
+
+    media = 0>=10;
+    a = a*2;
+    b = b*3;
+    c = c*5;
+    media = (a+b+c)/10;
+
+    printf("MEDIA = %.1lf\n", media);
+    return 0;
+}
