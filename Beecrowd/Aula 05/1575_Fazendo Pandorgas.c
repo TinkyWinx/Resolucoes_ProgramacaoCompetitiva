1a1fd0ef5ebcc643f6c94fb332412c6a70dfa6bc
⌞Winne⌝ ≽^• ˕ • ྀི≼ <reservawinne@gmail.com>
2026-09-09 22:28:07 -0300

    Update 1575_Fazendo Pandorgas.c

--- Beecrowd/Aula 05/1575_Fazendo Pandorgas.c
+++ Beecrowd/Aula 05/1575_Fazendo Pandorgas.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main (){
+  int quantpi, bambu1, bambu2, area, i;
+  
+  scanf("%d", &quantpi);
+  
+  for (i = 0 ; i < quantpi ; i++){
+    scanf ("%d%d", &bambu1, &bambu2);
+    
+    area = (bambu1*bambu2)/2;
+    
+    printf("%d cm2\n",area);
+  }
+  
+  return 0;
+}
